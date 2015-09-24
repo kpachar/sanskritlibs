d0ad4cb5275874d3be7f2e1471841ce03b5243d5
Pradyumna Achar <pradyumna.achar@gmail.com>
2015-09-24 19:47:53 +0530

    Sandhi - initial commit

---
+++ LanguageLibs/src/name/kpachar/sanskrit/lang/LangUtils.java
@@ -0,0 +1,14 @@
+package name.kpachar.sanskrit.lang;
+
+public class LangUtils {
+	/**
+	 * 
+	 * @param a A word
+	 * @param b Another word
+	 * @return The sandhi of a and b
+	 * <br />Eg: If a=प्राणी , b=ईश, sandhi(a,b)=प्राणेश
+	 */
+	public static String sandhi(String a, String b){
+		return null;
+	}
+}
