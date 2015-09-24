29502219781fca76c0284b1ac28f2aab176a6441
Pradyumna Achar <pradyumna.achar@gmail.com>
2015-09-24 21:05:19 +0530

    go to sleep now

---
+++ LanguageLibs/tests/name/kpachar/sanskrit/lang/LangUtilsTest.java
@@ -0,0 +1,16 @@
+package name.kpachar.sanskrit.lang;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LangUtilsTest {
+
+	@Test
+	public void testSandhi() {
+		assertEquals(LangUtils.sandhi("विष्णु", "पदे"), "विष्णुपदे");
+		assertEquals(LangUtils.sandhi("ईश", "पदे"), "ईशपदे");
+		assertEquals(LangUtils.sandhi("प्राणी", "ईश"), "प्राणेश");
+	}
+
+}
